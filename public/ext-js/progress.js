b3eb2045c22fd8700d2492f287c7709d51fe1ade
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-04-29 07:18:47 +0700

    ext-js: Fixup bandwidth limit calculation
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- public/ext-js/progress.js
+++ public/ext-js/progress.js
@@ -1,6 +1,5 @@
 var currentUsage = document.getElementById("progressValue").textContent;
 var limitUsage = 250;
-var totalUsage = (limitUsage - currentUsage);
-var totalUsagePercentage = (totalUsage*100)/limitUsage;
-document.getElementById("progressValue").innerText = totalUsage + "/250";
+var totalUsagePercentage = (currentUsage*100)/limitUsage;
+document.getElementById("progressValue").innerText = currentUsage + "/250";
 document.getElementById("progressBar").style.width = totalUsagePercentage.toFixed(2) + "%";
