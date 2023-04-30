4ca50c061af29c247000c87b6f2656edacf32df2
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-04-30 18:53:02 +0700

    ext-php: Use global environment and global function
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- public/ext-php/iLovePDFLimit.php
+++ public/ext-php/iLovePDFLimit.php
@@ -1,7 +1,7 @@
 <?php
     use Ilovepdf\Ilovepdf;
 
-    $ilovepdf = new Ilovepdf('project_public_325d386bc0c634a66ce67d65413fe30c_GE-Cv2861de258f64776f2928e69cb4868675','secret_key_a704c544b92db47bc422a824c6b3004e_QZVE20e592b1888ab4c21fca2f1b170b20f8b');
+    $ilovepdf = new Ilovepdf(env('ILOVEPDF_PUBLIC_KEY'),env('ILOVEPDF_SECRET_KEY'));
 
     //get remaining files
     $remainingFiles = $ilovepdf->getRemainingFiles();
