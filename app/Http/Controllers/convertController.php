89851953a6f7ba3b7bc70656835ef9fea3c7ac47
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-09-01 12:46:27 +0700

    Controller: Convert: Fixup for pdftoxls
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- app/Http/Controllers/convertController.php
+++ app/Http/Controllers/convertController.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 namespace App\Http\Controllers;
 
 use App\Helpers\AppHelper;
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Validator;
 use Ilovepdf\Ilovepdf;
 use Ilovepdf\PdfjpgTask;
 use Spatie\PdfToImage\Pdf;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class convertController extends Controller
 {
@@ -28,7 +30,7 @@ class convertController extends Controller
 			'file' => 'mimes:pdf|max:25000',
 			'fileAlt' => ''
 		]);
- 
+
 		if($validator->fails()) {
 			return redirect()->back()->withErrors($validator->messages())->withInput();
 		} else {
@@ -36,7 +38,7 @@ class convertController extends Controller
 			{
 				if($request->post('formAction') == "upload") {
 					if($request->hasfile('file')) {
-						$pdfUpload_Location = env('pdf_upload');
+						$pdfUpload_Location = env('PDF_UPLOAD');
 						$file = $request->file('file');
 						$file->move($pdfUpload_Location,$file->getClientOriginalName());
 						$pdfFileName = $pdfUpload_Location.'/'.$file->getClientOriginalName();
@@ -47,19 +49,19 @@ class convertController extends Controller
 							$pdf->setPage(1)
 								->setOutputFormat('png')
 								->width(400)
-								->saveImage(env('pdf_thumbnail'));
-							if (file_exists(env('pdf_thumbnail').'/1.png')) {
-								$thumbnail = file(env('pdf_thumbnail').'/1.png');
-								rename(env('pdf_thumbnail').'/1.png', env('pdf_thumbnail').'/'.$pdfNameWithoutExtension.'.png');
-								return redirect()->back()->with('upload','/'.env('pdf_thumbnail').'/'.$pdfNameWithoutExtension.'.png');
+								->saveImage(env('PDF_THUMBNAIL'));
+							if (file_exists(env('PDF_THUMBNAIL').'/1.png')) {
+								$thumbnail = file(env('PDF_THUMBNAIL').'/1.png');
+								rename(env('PDF_THUMBNAIL').'/1.png', env('PDF_THUMBNAIL').'/'.$pdfNameWithoutExtension.'.png');
+								return redirect()->back()->with('upload','/'.env('PDF_THUMBNAIL').'/'.$pdfNameWithoutExtension.'.png');
 							} else {
-								return redirect()->back()->withError('error',' has failed to upload !')->withInput();
+								return redirect()->back()->withErrors(['error'=>'Thumbnail file not found !'])->withInput();
 							}
 						} else {
-							return redirect()->back()->withError('error',' has failed to upload !')->withInput();
+							return redirect()->back()->withErrors(['error'=>'Thumbnail failed to generated !'])->withInput();
 						}
 					} else {
-						return redirect()->back()->withError('error',' FILE NOT FOUND !')->withInput();
+						return redirect()->back()->withErrors(['error'=>'PDF failed to upload !'])->withInput();
 					}
 				} else if ($request->post('formAction') == "convert") {
                     if(isset($_POST['convertType']))
@@ -68,7 +70,7 @@ class convertController extends Controller
 
                         if ($convertType == 'excel') {
                             if(isset($_POST['fileAlt'])) {
-                                $pdfUpload_Location = env('pdf_upload');
+                                $pdfUpload_Location = env('PDF_UPLOAD');
                                 $file = $request->post('fileAlt');
                                 $pdfProcessed_Location = 'temp';
                                 $pdfName = basename($file);
@@ -76,45 +78,48 @@ class convertController extends Controller
                                 $fileSize = filesize($file);
                                 $hostName = AppHelper::instance()->getUserIpAddr();
                                 $newFileSize = AppHelper::instance()->convert($fileSize, "MB");
-                    
+
                                 pdf_excel::create([
                                     'fileName' => $pdfName,
                                     'fileSize' => $newFileSize,
                                     'hostName' => $hostName
                                 ]);
-                    
-                                $pythonScripts = escapeshellcmd(env('PYTHON_EXECUTABLES').' ext-python\pdftoxlsx.py');
-                                $pythonRun = shell_exec($pythonScripts);
-                                if ($pythonRun = "true") {
+
+				                rename($pdfUpload_Location.'/'.$pdfName, $pdfUpload_Location.'/convert_xlsx.pdf');
+                                $process = new Process(['python', public_path().'/ext-python/pdftoxlsx.py'], null, ['SYSTEMROOT' => getenv('SYSTEMROOT'), 'PATH' => getenv("PATH")]);
+                                $process->run();
+
+                                if (!$process->isSuccessful()) {
+                                    return redirect()->back()->withErrors(['error'=>'Convert process error !'])->withInput();
+                                } else {
                                     if (file_exists($pdfProcessed_Location.'/converted.xlsx')) {
                                         $download_excel = $pdfProcessed_Location.'/converted.xlsx';
                                         return redirect()->back()->with('success',$download_excel);
                                     } else {
-                                        return redirect()->back()->withError('error',' has failed to convert !')->withInput();
+                                        return redirect()->back()->withErrors(['error'=>'Converted file not found !'])->withInput();
                                     }
-                                } else {
-                                    return redirect()->back()->withError('error',' has failed to convert !')->withInput();
                                 }
                             } else {
-                                return redirect()->back()->withError('error',' REQUEST NOT FOUND !')->withInput();
+                                return redirect()->back()->withErrors(['error'=>'INVALID_REQUEST_ERROR !'])->withInput();
                             }
                         } else if ($convertType == 'docx') {
                             if(isset($_POST['fileAlt'])) {
-                                $pdfUpload_Location = env('pdf_upload');
+                                $pdfUpload_Location = env('PDF_UPLOAD');
                                 $file = $request->post('fileAlt');
                                 $pdfProcessed_Location = 'temp';
                                 $pdfName = basename($file);
-                                $pdfNameWithoutExtension = basename($pdfName, ".pdf");
+				                $pdfNameInfo = pathinfo($file);
+                                $pdfNameWithoutExtension = $pdfNameInfo['filename'];
                                 $fileSize = filesize($file);
                                 $hostName = AppHelper::instance()->getUserIpAddr();
                                 $newFileSize = AppHelper::instance()->convert($fileSize, "MB");
-                        
+
                                 pdf_word::create([
                                     'fileName' => $pdfName,
                                     'fileSize' => $newFileSize,
                                     'hostName' => $hostName
                                 ]);
-                                
+
                                 $wordsApi = new WordsApi(env('ASPOSE_CLOUD_CLIENT_ID'), env('ASPOSE_CLOUD_TOKEN'));
                                 $uploadFileRequest = new UploadFileRequest($file, $pdfName);
                                 $wordsApi->uploadFile($uploadFileRequest);
@@ -122,7 +127,7 @@ class convertController extends Controller
                                     "save_format" => "docx",
                                     "file_name" => $pdfNameWithoutExtension.".docx",
                                 ));
-        
+
                                 $request = new SaveAsRequest(
                                     $pdfName,
                                     $requestSaveOptionsData,
@@ -133,22 +138,23 @@ class convertController extends Controller
                                 );
 
                                 $result = $wordsApi->saveAs($request);
+
                                 if (json_decode($result, true) !== NULL) {
-				    if (AppHelper::instance()->getFtpResponse($pdfProcessed_Location.'/'.$pdfNameWithoutExtension.".docx", $pdfNameWithoutExtension.".docx") == true) {
-				    	$download_word = $pdfProcessed_Location.'/'.$pdfNameWithoutExtension.".docx";
-			            	return redirect()->back()->with('success',$download_word);
-				    } else {
-					return redirect()->back()->withError('error',' has failed to convert !')->withInput();
-				    }
+                                    if (AppHelper::instance()->getFtpResponse($pdfProcessed_Location.'/'.$pdfNameWithoutExtension.".docx", $pdfNameWithoutExtension.".docx") == true) {
+                                        $download_word = $pdfProcessed_Location.'/'.$pdfNameWithoutExtension.".docx";
+                                        return redirect()->back()->with('success',$download_word);
+                                    } else {
+                                        return redirect()->back()->withErrors(['error'=>'FTP_CONNECTION_ERROR !'])->withInput();
+                                    }
                                 } else {
-                                    return redirect()->back()->withError('error',' has failed to convert !')->withInput();
+                                    return redirect()->back()->withErrors(['error'=>'Convert process error !'])->withInput();
                                 }
                             } else {
-                                return redirect()->back()->withError('error',' REQUEST NOT FOUND !')->withInput();
+                                return redirect()->back()->withErrors(['error'=>'INVALID_REQUEST_ERROR !'])->withInput();
                             }
                         } else if ($convertType == 'jpg') {
                             if(isset($_POST['fileAlt'])) {
-                                $pdfUpload_Location = env('pdf_upload');
+                                $pdfUpload_Location = env('PDF_UPLOAD');
                                 $file = $request->post('fileAlt');
                                 $pdfProcessed_Location = 'temp';
                                 $pdfName = basename($file);
@@ -156,13 +162,13 @@ class convertController extends Controller
                                 $fileSize = filesize($file);
                                 $hostName = AppHelper::instance()->getUserIpAddr();
                                 $newFileSize = AppHelper::instance()->convert($fileSize, "MB");
-                        
+
                                 pdf_jpg::create([
                                     'fileName' => $pdfName,
                                     'fileSize' => $newFileSize,
                                     'hostName' => $hostName
                                 ]);
-                    
+
                                 $ilovepdfTask = new PdfjpgTask(env('ILOVEPDF_PUBLIC_KEY'),env('ILOVEPDF_SECRET_KEY'));
                                 $ilovepdfTask->setFileEncryption(env('ILOVEPDF_ENC_KEY'));
                                 $pdfFile = $ilovepdfTask->addFile($file);
@@ -171,13 +177,13 @@ class convertController extends Controller
                                 $ilovepdfTask->setPackagedFilename($pdfNameWithoutExtension);
                                 $ilovepdfTask->execute();
                                 $ilovepdfTask->download($pdfProcessed_Location);
-        
+
                                 if(is_file($file)) {
                                     unlink($file);
                                 }
-                                
+
                                 $download_pdf = $pdfProcessed_Location.'/'.$pdfNameWithoutExtension.'.zip';
-        
+
                                 if (file_exists($download_pdf)) {
                                     return redirect()->back()->with('success',$download_pdf);
                                 } else {
@@ -185,23 +191,23 @@ class convertController extends Controller
                                     if (file_exists($download_pdf)) {
                                         return redirect()->back()->with('success',$download_pdf);
                                     } else {
-                                        return redirect()->back()->withError('error',' has failed to convert !')->withInput();
+                                        return redirect()->back()->withErrors(['error'=>'Convert process error !'])->withInput();
                                     }
                                 }
                             } else {
-                                return redirect()->back()->withError('error',' REQUEST NOT FOUND !')->withInput();
+                                return redirect()->back()->withErrors(['error'=>'INVALID_REQUEST_ERROR !'])->withInput();
                             }
                         } else {
-                            return redirect()->back()->withError('error','INVALID OPTION !')->withInput();
+                            return redirect()->back()->withErrors(['error'=>'REQUEST_ERROR_OUT_OF_BOUND !'])->withInput();
                         }
                     } else {
-                        return redirect()->back()->withError('error','MISSING VALUE !')->withInput();
+                        return redirect()->back()->withErrors(['error'=>'REQUEST_TYPE_NOT_FOUND !'])->withInput();
                     }
 				} else {
-					return redirect()->back()->withError('error',' FILE NOT FOUND !')->withInput();
+                    return redirect()->back()->withErrors(['error'=>'000'])->withInput();
 				}
 			} else {
-				return redirect()->back()->withError('error',' REQUEST NOT FOUND !')->withInput();
+                return redirect()->back()->withErrors(['error'=>'0x0'])->withInput();
 			}
 		}
 	}
--- public/ext-python/pdftoxlsx.py
+++ public/ext-python/pdftoxlsx.py
@@ -2,30 +2,31 @@
 import tabula
 import time
 import pandas as pd
-import os,sys, os.path
+import os,os.path
 from os import path
 
-pdf_input = r"C:\xampp\htdocs\emsitpro-pdftools-tailwind\public\upload-pdf"
-csv_input = r"C:\xampp\htdocs\emsitpro-pdftools-tailwind\public\temp-csv"
-pdf_output = r"C:\xampp\htdocs\emsitpro-pdftools-tailwind\public\temp-csv"
-csv_output = r"C:\xampp\htdocs\emsitpro-pdftools-tailwind\public\temp"
+os.chdir('..')
+pdf_input = r"C:\\Users\\Nickl\\Documents\\GitHub\\emsitpro-pdftools-tailwind\\public\\upload-pdf"
+csv_input = r"C:\\Users\\Nickl\\Documents\\GitHub\\emsitpro-pdftools-tailwind\\public\\temp-csv"
+pdf_output = r"C:\\Users\\Nickl\\Documents\\GitHub\\emsitpro-pdftools-tailwind\\public\\temp-csv"
+csv_output = r"C:\\Users\\Nickl\\Documents\\GitHub\\emsitpro-pdftools-tailwind\\public\\temp"
 
-df = tabula.read_pdf(pdf_input+"\\convert_xlsx.pdf", pages='all')[0]
-tabula.convert_into(pdf_input+"\\convert_xlsx.pdf", pdf_output+"\\converted.csv", output_format="csv", pages='all')
+df = tabula.read_pdf(pdf_input+"\convert_xlsx.pdf", encoding='utf-8', multiple_tables=True, pages="all")
+tabula.convert_into(pdf_input+"\convert_xlsx.pdf", pdf_output+"\converted.csv", output_format="csv", pages="all")
 time.sleep(2.5)
 
-if os.path.isfile(csv_input+"\\converted.csv"):
-        read_csv_file = pd.read_csv (csv_input+"\\converted.csv")
-        read_csv_file.to_excel (csv_output+"\\converted.xlsx", index = None, header=True)
-        if os.path.isfile(csv_output):
-            os.remove(pdf_input+"\\convert_xlsx.pdf")
-            os.remove(csv_input+"\\converted.csv")
+if os.path.isfile(csv_input+"\converted.csv"):
+        read_csv_file = pd.read_csv (csv_input+"\converted.csv", on_bad_lines='skip')
+        read_csv_file.to_excel (csv_output+"\converted.xlsx",header=True)
+        if os.path.isfile(csv_output+"\converted.xlsx"):
+            os.remove(pdf_input+"\convert_xlsx.pdf")
+            os.remove(csv_input+"\converted.csv")
             print("true")
         else:
-            os.remove(pdf_input+"\\convert_xlsx.pdf")
-            os.remove(csv_input+"\\converted.csv")
+            os.remove(pdf_input+"\convert_xlsx.pdf")
+            os.remove(csv_input+"\converted.csv")
             print("false")
 else:
-    os.remove(pdf_input+"\\convert_xlsx.pdf")
-    os.remove(csv_input+"\\converted.csv")
+    os.remove(pdf_input+"\convert_xlsx.pdf")
+    os.remove(csv_input+"\converted.csv")
     print("false")
