05b1b4871316e477d2d208ebd5f19fa174139286
eureka <eureka@vps.hana-ci>
2023-09-10 23:42:26 +0700

    header: Fixup manual book error
    * Set another folder name to "guide", instead of "manual" that already use for
    apache 2.4 manual documentation
    
    Signed-off-by: eureka <eureka@vps.hana-ci>

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -60,7 +60,7 @@
         </div>
         <!-- Modal footer -->
         <div class="flex items-center space-x-2 rounded-b border-t border-gray-200 p-6 dark:border-gray-600">
-          <button data-modal-hide="defaultModal" type="button" class="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><a href="manual/manual_book_20230903_rev1.pdf">Get Manual Book</a></button>
+          <button data-modal-hide="defaultModal" type="button" class="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><a href="guide/manual_book_20230903_rev1.pdf">Get Manual Book</a></button>
           <button data-modal-hide="defaultModal" type="button" class="rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-900 focus:z-10 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-gray-500 dark:bg-gray-700 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-600">Skip</button>
         </div>
       </div>
