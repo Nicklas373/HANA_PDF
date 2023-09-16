ed0b9611fb1c55d4e8a141752087e71735f2444f
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-09-16 14:08:10 +0700

    config: promote dev/master-live as production
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://pdf.hana-ci.com'),
+    'url' => env('APP_URL', 'https://pdf.hana-ci.com'),
 
     'asset_url' => env('ASSET_URL'),
 
@@ -70,7 +70,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'Asia/Jakarta',
 
     /*
     |--------------------------------------------------------------------------
