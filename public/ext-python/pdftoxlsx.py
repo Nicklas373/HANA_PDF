f28dbea5d5d6c916fb6c708613e58b57d95d4179
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-10-08 01:07:08 +0700

    Controllers: Convert: Use symlinks laravel storage
    - Drop directories leaks from convert view
    - Move all public files into laravel symlink storage
    - Use hash code for filename instead of original name
    - Update directories for python script
    
    Documentation: https://laravel.com/docs/10.x/filesystem
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- public/ext-python/pdftoxlsx.py
+++ public/ext-python/pdftoxlsx.py
@@ -6,17 +6,17 @@ import os,os.path
 from os import path
 
 os.chdir('..')
-pdf_input = r"C:\\Users\\Nickl\\Documents\\GitHub\\emsitpro-pdftools-tailwind\\public\\upload-pdf"
-csv_input = r"C:\\Users\\Nickl\\Documents\\GitHub\\emsitpro-pdftools-tailwind\\public\\temp-csv"
-pdf_output = r"C:\\Users\\Nickl\\Documents\\GitHub\\emsitpro-pdftools-tailwind\\public\\temp-csv"
-csv_output = r"C:\\Users\\Nickl\\Documents\\GitHub\\emsitpro-pdftools-tailwind\\public\\temp"
+pdf_input = r"C:\\Users\\Nickl\\Documents\\GitHub\\emsitpro-pdftools-tailwind\\storage\\app\\public\\upload-pdf"
+csv_input = r"C:\\Users\\Nickl\\Documents\\GitHub\\emsitpro-pdftools-tailwind\\storage\\app\\public\\temp-csv"
+pdf_output = r"C:\\Users\\Nickl\\Documents\\GitHub\\emsitpro-pdftools-tailwind\\storage\\app\\public\\temp-csv"
+csv_output = r"C:\\Users\\Nickl\\Documents\\GitHub\\emsitpro-pdftools-tailwind\\storage\\app\\public\\temp"
 
 df = tabula.read_pdf(pdf_input+"\convert_xlsx.pdf", encoding='utf-8', multiple_tables=True, pages="all")
 tabula.convert_into(pdf_input+"\convert_xlsx.pdf", pdf_output+"\converted.csv", output_format="csv", pages="all")
 time.sleep(2.5)
 
 if os.path.isfile(csv_input+"\converted.csv"):
-        read_csv_file = pd.read_csv (csv_input+"\converted.csv", on_bad_lines='skip')
+        read_csv_file = pd.read_csv (csv_input+"\converted.csv", on_bad_lines='skip', encoding = 'unicode_escape')
         read_csv_file.to_excel (csv_output+"\converted.xlsx",header=True)
         if os.path.isfile(csv_output+"\converted.xlsx"):
             os.remove(pdf_input+"\convert_xlsx.pdf")
