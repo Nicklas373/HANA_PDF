95f1612246b36555d56baec1c80d3c830f1dcd1e
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-11-05 23:25:42 +0700

    README: update documentation
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- resources/views/pages/cnvToPDF.blade.php
+++ resources/views/pages/cnvToPDF.blade.php
@@ -104,7 +104,7 @@
                                 $pdfFileAppend = session('pdfRndmName');
                                 $pdfThumbAppend = session('pdfThumbName');
                                 $pdfRealName = session('pdfOriName');
-				$pdfLink = "https://docs.google.com/viewerng/viewer?url=https://pdf.hana-ci.com".$pdfFileAppend."&embedded=true";
+				                $pdfLink = "https://docs.google.com/viewerng/viewer?url=https://pdf.hana-ci.com".$pdfFileAppend."&embedded=true";
                                 echo '
                                     <label id="PreviewName" class="block mb-2 font-poppins text-base font-semibold text-slate-900">Preview '.$pdfRealName.'</label>
                                     <input type="text" id="fileAlt" name="fileAlt" class="" placeholder="" style="display: none;" value="'.$pdfFileAppend.'">
