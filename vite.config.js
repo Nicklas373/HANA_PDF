1037cbf3138b771173ca7fbd6d6a716d222be101
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-11-05 21:15:21 +0700

    vite: Include flowbite custom js as static assets

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,14 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/flowbite.js'],
             refresh: true,
         }),
     ],
     server: {
-        host: true
+        host: true,
+        hmr: {
+            host: 'localhost'
+        },
     }
 });
