f1d83408f21fb5cdff2a2eab95066c7a6d45be57
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-11-05 21:36:14 +0700

    resources: Do not use static asset on local build
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -3,6 +3,4 @@
 <title>HANA PDF</title>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.css" rel="stylesheet" />
 <link rel="stylesheet" href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css" />
-<link rel="stylesheet" href="{{ asset('build/assets/app-93b9d882.css') }}" />
-<link rel="script" href="{{ asset('build/assets/app-c1194546.js')}}" />
-<link rel="script" href="{{ asset('build/assets/index-f475cd16.js')}}" />
+@vite(['resources/css/app.css','resources/js/app.js','resources/js/flowbite.js'])
