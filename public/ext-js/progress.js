2fdfa1d22b1df38b83b62444cf0c2b0567535254
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-11-17 16:57:59 +0700

    ext-js: Merge all separate js files into single js
    * Merge all separate js controller into single main js script

--- public/ext-js/progress.js
+++
@@ -1,9 +0,0 @@
-function init() {
-    var currentUsage = document.getElementById("progressValue").textContent;
-    var limitUsage = 250;
-    var totalUsagePercentage = (currentUsage*100)/limitUsage;
-    document.getElementById("progressValue").innerText = currentUsage + "/250";
-    document.getElementById("progressBar").style.width = totalUsagePercentage.toFixed(2) + "%";
-};
-
-init();
