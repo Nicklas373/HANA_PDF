d5ef003a1ee83801f82bcaf21aba43174a64bdb8
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-11-17 18:14:11 +0700

    resources: includes: set again
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta name="theme-color" content="#DEE7ED">
+<meta name="theme-color" content="#dee7ed">
 <title>HANA PDF</title>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.css" rel="stylesheet" />
 <link rel="stylesheet" href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css" />
