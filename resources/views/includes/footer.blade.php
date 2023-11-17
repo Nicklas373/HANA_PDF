1c477bc8b6ec8703e17a5e89cb917016a157fb80
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-11-17 17:07:02 +0700

    Treewide: includes: Tailwind tidy up !
    * Do tailwind tidy up to fix code in-efficient issue
    * Fixup few bunch of warning from browser logs
    
    References:
    - https://play.tailwindcss.com/

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,3 @@
-<div class="fixed bottom-0 left-0 right-0 w-full backdrop-blur-md bg-opacity-50 bg-slate-300 z-10 p-2 shadow md:flex md:items-center md:justify-between">
+<div class="fixed bottom-0 left-0 right-0 z-10 w-full bg-slate-300 bg-opacity-50 p-2 shadow backdrop-blur-md md:flex md:items-center md:justify-between">
     <span class="font-poppins rounded font-semibold text-slate-700">© 2023 <a href="https://github.com/HANA-CI-Build-Project" class="hover:underline">HANA-CI Build Project</a>. All Rights Reserved.</span>
   </div>
