83f5c812360fc6a7b95b80f78548a00242bef189
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-11-17 17:17:35 +0700

    Console: Kernel: Introduce Laravel Scheduler
    * Introduce some of scheduler that will do cleanup for public storage and web cache with specific declared time, and log into datalogs

---
+++ app/Console/Commands/StorageCleanup.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class StorageCleanup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'hana:clear-storage';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Cleanup linked storage every hours';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $pdfUpload_Location = env('PDF_UPLOAD');
+        $pdfProcessed_Location = env('PDF_DOWNLOAD');
+        $pdfMerge_Location = env('PDF_MERGE_TEMP');
+        $publicUploadTemp = Storage::allFiles('public/'.$pdfUpload_Location);
+        $publicDownloadTemp = Storage::allFiles('public/'.$pdfProcessed_Location);
+        $publicMergeTemp = Storage::allFiles('public/'.$pdfMerge_Location);
+        Storage::delete($publicUploadTemp);
+        Storage::delete($publicDownloadTemp);
+        Storage::delete($publicMergeTemp);
+    }
+}
