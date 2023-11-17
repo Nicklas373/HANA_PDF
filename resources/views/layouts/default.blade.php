5e189fc176dca8ead3926046af8a04e1aa07e2b3
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-11-17 17:06:13 +0700

    layouts: default: Fixup repeat background

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,13 +3,13 @@
   <head>
     @include('includes.head')
     <header>@include('includes.header')</header>
-    <script src="/ext-js/dropdown.js"></script>
   </head>
-  <body style="background-image: url('../assets/whitebg.png');">
+  <body class="bg-cover bg-fixed bg-center bg-no-repeat" style="background-image: url('../assets/whitebg.png');">
     <div>
       <div id="content">
-        @yield('content')
-        <script src="/build/assets/flowbite-44f515b4.js" type="module"></script>
+        @include('includes.modal') @yield('content')
+        <!--<script src="/build/assets/flowbite-5fb3e959.js" type="module"></script>-->
+        <script src="/ext-js/kao-main.js"></script>
         <script src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script>
       </div>
       <footer>@include('includes.footer')</footer>
