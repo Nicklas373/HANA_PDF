dbe127eb42e853c2723c7119331bfa18537c58fb
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-11-23 21:57:47 +0700

    vite: update prebuild assets
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -5,5 +5,5 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.css" rel="stylesheet" />
 <link rel="stylesheet" href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css" />
 <link rel="stylesheet" href="{{ asset('build/assets/app-86b5075b.css') }}" />
-<link rel="script" href="{{ asset('build/assets/app-c1194546.js')}}" />
+<link rel="script" href="{{ asset('build/assets/app-17ddd97f.css')}}" />
 <link rel="script" href="{{ asset('build/assets/index-f475cd16.js')}}" />
