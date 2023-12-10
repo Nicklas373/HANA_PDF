005377abcc0711a9f3467059749bcf00bd292226
eureka <eureka@hana.ci>
2023-12-10 11:56:34 +0700

    includes: remove unused css
    Signed-off-by: eureka <eureka@hana.ci>

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,8 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="theme-color" content="#dee7ed">
 <title>HANA PDF</title>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css" rel="stylesheet" />
-<link rel="stylesheet" href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css" />
 <link rel="stylesheet" href="{{ asset('build/assets/app-088b0bbd.css')}}" />
 <link rel="script" href="{{ asset('build/assets/app-6ceaad5c.js') }}" />
 <link rel="script" href="{{ asset('build/assets/index-ccf1ca32.js')}}" />
