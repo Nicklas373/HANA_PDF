5abf45b67d28fc030504e30bcb4ce1e22ebdfa58
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-12-10 12:05:42 +0700

    vite: switch to static assets for test
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,6 +2,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="theme-color" content="#d1d5db">
 <title>HANA PDF</title>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.css" rel="stylesheet" />
-<link rel="stylesheet" href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css" />
-@vite(['resources/css/app.css','resources/js/app.js','resources/js/kao-logic.js'])
+<!-- Wrap this on comment when on development server ! -->
+<link rel="stylesheet" href="{{ asset('build/assets/app-088b0bbd.css')}}" />
+<link rel="script" href="{{ asset('build/assets/app-6ceaad5c.js') }}" />
+<link rel="script" href="{{ asset('build/assets/index-ccf1ca32.js')}}" />
+<!-- Only use VITE assets while on development server, uncomment and switch to @ vite from vite@
+vite@(['resources/css/app.css','resources/js/app.js','resources/js/kao-logic.js'])-->
