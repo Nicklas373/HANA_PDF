0b27e5fa2d5e8a284a72f342c13346348b2264dc
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-12-26 20:21:03 +0700

    treewide: Update library and dependencies
    - Bump vite, axios, laravel-vite-plugin and other to the latest version
    - Configure all ECMAScript module (ESM) file that use 'require' to adapt with new vite 5.1.0 dependency
    to use 'from'
    - Declare "type": "module" on package.json
    - Fixup warning 'Unknown at rule @tailwindcss(unknownAtRules)' by properly declare tailwind import on app.css
    - Fixup warning 'Consider adding preconnect or dns-prefetch resource hints to establish early connections to
    important third-party origins. Learn how to preconnect to required origins.FCPLCP' from Google Lighthouse by
    declare properly google fonts on css
    - Re-built all static library after update on vite to version 5.1.0
    - Re-configure npm library version to adapt properly with latest vite version
    
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,14 +4,13 @@
     @include('includes.head')
     <header>@include('includes.header')</header>
   </head>
-  <body class="bg-cover bg-fixed bg-center bg-no-repeat" style="background-image: url('../assets/whitebg.png');">
+  <body class="bg-cover bg-fixed bg-center bg-no-repeat" style="background-image: url('../assets/whitebg.avif');">
     <div>
       <div id="content" class="flex flex-col min-h-screen justify-between">
         @include('includes.modal') @yield('content')
-        <!--<script src="/build/assets/flowbite-5fb3e959.js" type="module"></script>-->
-        <script src="/ext-js/kao-controller.js"></script>
-        <script src="/ext-js/kao-main.js"></script>
-        <script src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script>
+        <script async src="/ext-js/kao-controller.js"></script>
+        <script async src="/ext-js/kao-main.js"></script>
+        <script async src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script>
       </div>
       <footer>@include('includes.footer')</footer>
     </div>
