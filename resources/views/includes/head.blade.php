6e266cb63e7db92397bfff04f9ce6e9fd44ab7a3
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-12-26 20:37:27 +0700

    includes: Drop unused css
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,6 +2,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="theme-color" content="#d1d5db">
 <title>HANA PDF</title>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.css" rel="stylesheet" />
 <link rel="stylesheet" href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css" />
 @vite(['resources/css/app.css','resources/js/app.js','resources/js/kao-logic.js'])
