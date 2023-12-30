b4815d151c14bcfc006f683c7acfea15fbe6323a
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2023-12-30 13:10:20 +0700

    Dockerfile: Finalize docker workflow
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- module/bash/docker-build-info.sh
+++ module/bash/docker-build-info.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-curdate=$(date)
+TG_TOKEN=$1
+TG_CHAT_ID=$2
+CONTAINER_NAME=$3
+WORKFLOW_URL=$4
+IMAGE_NAME=$5
+STATUS=$6
+curdate=$(TZ=Indonesia/Jakarta date)
 
 function bot_template() {
-curl -s -X POST https://api.telegram.org/$1/sendMessage -d chat_id=$2 -d "parse_mode=HTML" -d text="$(
+curl -s -X POST https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d "parse_mode=HTML" -d text="$(
 	for POST in "${@}";
 		do
 			echo "${POST}"
@@ -18,10 +24,11 @@ function bot_message() {
 			"<b> Github Workflow Start ! </b>" \
 			"" \
 			"============= Job Information ================" \
-			"<b>Workflow :</b><code> $3 </code>" \
-			"<b>Workflow Detail :</b><code><a href='$4'></a></code>" \
+			"<b>Workflow :</b><code> $CONTAINER_NAME </code>" \
+			"<b>Workflow Detail :</b><code> <a href='$WORKFLOW_URL'>$WORKFLOW_URL</a> </code>" \
 			"<b>Workflow Environment :</b><code> Production </code>" \
-			"<b>Workflow Step :</b><code> $5 </code>" \
+			"<b>Workflow Status :</b><code> $STATUS </code>" \
+			"<b>Workflow Step :</b><code> $IMAGE_NAME </code>" \
 			"<b>Workflow Start At :</b><code> ${curdate} </code>" \
 			""
 }
