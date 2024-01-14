e3da7cd1d2f9a0939130ecb8a35ed9a5606291e1
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2024-01-14 08:07:06 +0700

    Vite: update pre-built library
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,6 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="theme-color" content="#dee7ed">
 <title>HANA PDF</title>
-<link rel="stylesheet" href="{{ asset('build/assets/app-VwWTePit.css')}}" />
-<link rel="script" href="{{ asset('build/assets/app-kNoGxclM.js') }}" />
+<link rel="stylesheet" href="{{ asset('build/assets/app-l6JlhLPd.css')}}" />
+<link rel="script" href="{{ asset('build/assets/app-DBpsIzgy.js') }}" />
 <link rel="script" href="{{ asset('build/assets/index-Qkg2JGee.js')}}" />
