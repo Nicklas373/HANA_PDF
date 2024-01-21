4a04f36d14d94703eb9254d63883705eb06bd9ef
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2024-01-21 19:36:37 +0700

    database: Introduce several table after last patch

---
+++ database/migrations/2024_01_01_000008_create_datalogs_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * The database connection that should be used by the migration.
+     *
+     * @var string
+     */
+    protected $connection = 'pgsql';
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobLogs', function (Blueprint $table) {
+            $table->id('jobsId');
+            $table->string('jobsName', 25);
+            $table->string('jobsEnv', 25);
+            $table->string('jobsRuntime', 25);
+            $table->boolean('jobsResult');
+            $table->timestamp('procStartAt')->nullable();
+            $table->timestamp('procEndAt')->nullable();
+            $table->text('procDuration')->nullable();
+            $table->uuid('processId');
+
+            // Configure foreign key
+            $table->foreign('processId')->references('processId')->on('appLogs');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobLogs');
+    }
+};
