30453e52d10b6293b364f179c8cf22d0ee91cac8
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2024-01-21 22:31:07 +0700

    migrations: Fixup migrations order
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,3 @@
 <div class=" w-full bg-slate-300 bg-opacity-50 p-2 shadow backdrop-blur-md md:flex md:items-center md:justify-between">
-    <span class="font-poppins rounded font-semibold text-slate-700">© 2023 <a href="https://github.com/HANA-CI-Build-Project" class="hover:underline">HANA-CI Build Project</a>. All Rights Reserved.</span>
+    <span class="font-poppins rounded font-semibold text-slate-700">© 2024 <a href="https://github.com/HANA-CI-Build-Project" class="hover:underline">HANA-CI Build Project</a>. All Rights Reserved.</span>
   </div>
