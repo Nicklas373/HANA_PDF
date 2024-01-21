bf17c2b58c02e0151f5e8aefdb30151ca6933869
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2024-01-21 19:31:01 +0700

    App: Enhance exceptions handler
    - Properly handling when user access unwanted page or wrong request method, instead showing server error page. It should return to page 404 or any that should be return page
    - Send log to telegram and database whenever this incident happen for better monitoring

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,14 @@
 
 namespace App\Exceptions;
 
+use App\Helpers\AppHelper;
+use App\Helpers\NotificationHelper;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Session\TokenMismatchException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +33,71 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        $uuid = AppHelper::Instance()->get_guid();
+        $userIp = $request->ip();
+
+        try {
+            $currentRoute = $request->route();
+            $currentRouteInfo = $currentRoute ? $currentRoute->uri() : 'No route information available';
+            Log::info($currentRouteInfo);
+        } catch (\Exception $e) {
+            Log::error('Failed to retrieve current route: ' . $e->getMessage());
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            $uri = $request->path();
+            $route = $request->route();
+            if ($route && method_exists($route, 'methods')) {
+                $methods = $route->methods();
+                $message = 'MethodNotAllowedHttpException for route: ' . $uri . ' (' . implode(', ', $methods) . ')';
+                NotificationHelper::Instance()->sendRouteErrNotify($uuid, 'FAIL', 'Method not allowed exception!', $currentRouteInfo, $message, $userIp);
+                try {
+                    DB::table('accessLogs')->insert([
+                        'processId' => $uuid,
+                        'routePath' => $currentRouteInfo,
+                        'accessIpAddress' => true,
+                        'routeExceptionMessage' => 'Method not allowed exception!',
+                        'routeExceptionLog' => $message
+                    ]);
+                } catch (QueryException $ex) {
+                    Log::error('Query Exception failed with: '. $e->getMessage());
+                }
+            } else {
+                $message = 'MethodNotAllowedHttpException for route: ' . $uri;
+                NotificationHelper::Instance()->sendRouteErrNotify($uuid, 'FAIL', 'Method not allowed exception!', 'null', $message, $userIp);
+                try {
+                    DB::table('accessLogs')->insert([
+                        'processId' => $uuid,
+                        'routePath' => null,
+                        'accessIpAddress' => true,
+                        'routeExceptionMessage' => 'Method not allowed exception!',
+                        'routeExceptionLog' => $message
+                    ]);
+                } catch (QueryException $ex) {
+                    Log::error('Query Exception failed with: '. $e->getMessage());
+                }
+            }
+            abort(404);
+        } else if ($exception instanceof TokenMismatchException || ($exception instanceof HttpException && $exception->getStatusCode() == 419)) {
+             $message = 'TokenMismatchException: ' . $exception->getMessage();
+             NotificationHelper::Instance()->sendRouteErrNotify($uuid, 'FAIL', 'Token Mismatch exception!', $currentRouteInfo, $message, $userIp);
+             try {
+                DB::table('accessLogs')->insert([
+                    'processId' => $uuid,
+                    'routePath' => $currentRouteInfo,
+                    'accessIpAddress' => true,
+                    'routeExceptionMessage' => 'Token Mismatch exception!',
+                    'routeExceptionLog' => $message
+                ]);
+            } catch (QueryException $ex) {
+                Log::error('Query Exception failed with: '. $e->getMessage());
+            }
+            abort(419);
+        }
+        return parent::render($request, $exception);
     }
 }
