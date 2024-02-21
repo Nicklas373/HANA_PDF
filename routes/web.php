9a2575be3d43b7daa62a56aaae27376956c0e170
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2024-02-21 21:49:24 +0700

    Routes: Update new route
    Re-define some of routes into new route after REST API implementation

--- routes/web.php
+++ routes/web.php
@@ -60,20 +60,25 @@ Route::get('/watermark', function()
    return View::make('pages.watermark');
 });
 
-// API CSRF Token
-Route::get('/api/v1/token', 'App\Http\Controllers\tokenController@getToken');
+// API v1 CSRF Token
+Route::get('/api/v1/token', 'App\Http\Controllers\main\tokenController@getToken');
 
-// API Processing Route
-Route::post('/api/v1/proc/compress', 'App\Http\Controllers\proc\compressController@compress');
-Route::post('/api/v1/proc/convert', 'App\Http\Controllers\proc\convertController@convert');
-Route::post('/api/v1/proc/html', 'App\Http\Controllers\proc\htmltopdfController@html');
-Route::post('/api/v1/proc/merge', 'App\Http\Controllers\proc\mergeController@merge');
-Route::post('/api/v1/proc/split', 'App\Http\Controllers\proc\splitController@split');
-Route::post('/api/v1/proc/watermark', 'App\Http\Controllers\proc\watermarkController@watermark');
-
-// API Backend Logging Route
+// API v1 Backend Logging Route
 Route::post('/api/v1/logs/limit', 'App\Http\Controllers\data\limitLogController@getLimit');
 Route::post('/api/v1/logs/proc/single', 'App\Http\Controllers\data\processLogController@getLogs');
 Route::post('/api/v1/logs/proc/all', 'App\Http\Controllers\data\processLogController@getAllLogs');
 Route::post('/api/v1/logs/notify/single', 'App\Http\Controllers\data\notifyLogController@getLogs');
 Route::post('/api/v1/logs/notify/all', 'App\Http\Controllers\data\notifyLogController@getAllLogs');
+
+// API v1 Upload Route
+Route::post('api/v1/file/upload', 'App\Http\Controllers\main\uploadController@upload');
+Route::post('api/v1/file/remove', 'App\Http\Controllers\main\uploadController@remove');
+Route::post('api/v1/file/thumbnail', 'App\Http\Controllers\main\thumbnailController@getThumbnail');
+
+// API v2 Processing Route
+Route::post('api/v2/proc/compress', 'App\Http\Controllers\proc\compressController@compress');
+Route::post('api/v2/proc/convert', 'App\Http\Controllers\proc\convertController@convert');
+Route::post('api/v2/proc/html', 'App\Http\Controllers\proc\htmltopdfController@html');
+Route::post('api/v2/proc/merge', 'App\Http\Controllers\proc\mergeController@merge');
+Route::post('api/v2/proc/split', 'App\Http\Controllers\proc\splitController@split');
+Route::post('api/v2/proc/watermark', 'App\Http\Controllers\proc\watermarkController@watermark');
