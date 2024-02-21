9e6808afe5c8578ba803777285253f56c35a0edd
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2024-02-21 21:50:06 +0700

    resources: views: Design all custom error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,19 @@
-@extends('errors::minimal')
-
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', __('Not Found'))
+<html lang="en">
+@extends('layouts.clean-layout')
+@section('content')
+<div id="404">
+    <section class="flex flex-col justify-center items-center bg-dt1 bg-opacity-35 backdrop-filter backdrop-blur-md w-full h-full">
+        <div class="text-center px-2">
+            <h1 class="text-5xl lg:text-8xl font-semibold font-quicksand text-lt1">404</h1>
+        </div>
+        <div class="text-center px-4">
+            <h2 class="mt-4 lg:mt-8 text-lg lg:text-3xl font-medium font-quicksand text-lt2"> Page not found</h2>
+            <h2 class="mt-4 text-xs text-start lg:text-lg font-medium font-quicksand text-lt4">The page that you are looking for does not exist</h2>
+            <h2 class="text-xs lg:text-lg text-center font-medium font-quicksand text-lt4">It may already move or deleted permanently.</h3>
+        </div>
+        <div class="mt-10 px-4">
+            <button type="button" id="submitBtn_1" class="p-4 font-quicksand font-medium bg-rt1 text-lt1 rounded-lg cursor-pointer w-full h-14" onclick="returnToHome()" data-ripple-light="true">Go Back Home</button>
+        </div>
+    </section>
+</div>
+@stop
