3f4a0b4c46d128acc6b2b6f64f56d7f5d4b9bce1
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2024-02-24 10:11:18 +0700

    resources: Fixup case-sensitive issue
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- resources/views/layouts/alternate-layout.blade.php
+++ resources/views/layouts/alternate-layout.blade.php
@@ -9,15 +9,9 @@
     <div id="content" class="flex flex-col min-h-screen justify-between">
         @yield('content')
     </div>
-<<<<<<< HEAD
-=======
-    <script async src="/build/assets/kao-logic-D2YWooi_.js" type="module"></script>
->>>>>>> 65f31d1 (Treewide: Minor changes)
     <script async type="text/javascript" src="/ext-js/kao-controller.js"></script>
     <script async type="text/javascript" src="/ext-js/kao-main.js"></script>
     <script async type="text/javascript" src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script>
-    <script async type="module" src="/pdfjs/build/pdf.mjs"></script>
-    <script async type="module" src="/pdfjs/build/pdf.worker.mjs"></script>
   </body>
   <footer>@include('includes.footer')</footer>
 </html>
