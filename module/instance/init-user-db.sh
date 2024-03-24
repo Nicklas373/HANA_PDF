58047409db37950461e0adb808b0c33db665d381
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2024-03-25 05:20:38 +0700

    Docker: Include integration
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

---
+++ module/instance/init-user-db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER eureka WITH PASSWORD 'eureka';
+    GRANT USAGE, CREATE ON SCHEMA public TO eureka;
+    ANAYLYZE;
+EOSQL
