7a5d974974698bca1967df35e40315dd8a561f8c
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2024-03-24 23:01:30 +0700

    Controllers: Fixup python init
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- app/Http/Controllers/Api/Core/convertController.php
+++ app/Http/Controllers/Api/Core/convertController.php
@@ -147,7 +147,7 @@ class convertController extends Controller
                     $pdfNameWithExtension = pathinfo($currentFileName, PATHINFO_EXTENSION);
                     if ($convertType == 'xlsx') {
                         $asposeAPI = new Process([
-                            'python',
+                            'python3',
                             public_path().'/ext-python/asposeAPI.py',
                             env('ASPOSE_CLOUD_CLIENT_ID'),
                             env('ASPOSE_CLOUD_TOKEN'),
@@ -572,7 +572,7 @@ class convertController extends Controller
                         }
                     } else if ($convertType == 'pptx') {
                         $asposeAPI = new Process([
-                            'python',
+                            'python3',
                             public_path().'/ext-python/asposeAPI.py',
                             env('ASPOSE_CLOUD_CLIENT_ID'),
                             env('ASPOSE_CLOUD_TOKEN'),
