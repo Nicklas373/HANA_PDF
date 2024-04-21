b23855df7282b7cf26097fd28d041ae33a1bcde8
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2024-04-21 14:47:14 +0700

    resources: Implement simple meta tag
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- resources/views/includes/head-lite.blade.php
+++ resources/views/includes/head-lite.blade.php
@@ -1,6 +1,13 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="theme-color" content="#d1d5db">
+<meta name="author" content="Dicky Herlambang"/>
+<meta name="application-name" content="HANA PDF">
+<meta name="copyright" content="HANA-CI Build Project">
+<meta name="description" content="HANA PDF is your go-to solution for effortlessly managing your PDFs. We've designed it with simplicity in mind, so you can edit, combine, shrink, convert, and personalize your PDFs with just a few clicks."/>
+<meta name="keywords" content="PDF, PDF Compress, PDF Convert, PDF Tools, PDF Tool"/>
+<meta name='robots' content="index, follow">
+<meta name='revisit-after' content="3 days">
 <title>HANA PDF</title>
 <link rel="icon" type="image/svg" href="{{ asset('assets/logo/ico.svg') }}">
 @vite(['resources/css/app-lite.css','resources/css/font.css'])
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,6 +1,13 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="theme-color" content="#dee7ed">
+<meta name="author" content="Dicky Herlambang"/>
+<meta name="application-name" content="HANA PDF">
+<meta name="copyright" content="HANA-CI Build Project">
+<meta name="description" content="HANA PDF is your go-to solution for effortlessly managing your PDFs. We've designed it with simplicity in mind, so you can edit, combine, shrink, convert, and personalize your PDFs with just a few clicks."/>
+<meta name="keywords" content="PDF, PDF Compress, PDF Convert, PDF Tools, PDF Tool"/>
+<meta name='robots' content="index, follow">
+<meta name='revisit-after' content="3 days">
 <title>HANA PDF</title>
 <link rel="icon" type="image/svg" href="{{ asset('assets/logo/ico.svg') }}">
 <link rel="stylesheet" type="text/css" href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css" />
