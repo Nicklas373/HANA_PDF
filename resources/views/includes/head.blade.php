566751b72f6bd36c64730867ad1b14b36be7069e
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2024-06-01 11:42:35 +0700

    kao-logic: Minor fixes
    - Fix thumbnail are not loaded properly for PPTX or PPT file extension
    - Properly set dropzone to version 6.0.0-beta2
    
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -11,5 +11,5 @@
 <title>HANA PDF</title>
 <link rel="icon" type="image/svg" href="{{ asset('assets/logo/ico.svg') }}">
 <link rel="stylesheet" type="text/css" href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css" />
-<link rel="stylesheet" type="text/css" href="https://unpkg.com/dropzone@6.0.0-beta.1/dist/dropzone.css" />
+<link rel="stylesheet" type="text/css" href="https://unpkg.com/dropzone@6.0.0-beta.2/dist/dropzone.css" />
 @vite(['resources/css/app.css','resources/css/font.css','resources/js/app.js','resources/js/kao-logic.js'])
