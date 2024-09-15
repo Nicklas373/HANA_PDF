64618e965f01525c20682544f139e51f8fca1449
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2024-09-15 14:40:06 +0700

    vite: Include CORS flags
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,28 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import getGitCommitHash from './resources/js/getGitCommitHash';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import getGitCommitHash from "./resources/js/getGitCommitHash";
 
 export default defineConfig({
     define: {
-        gitHash: JSON.stringify(getGitCommitHash())
+        gitHash: JSON.stringify(getGitCommitHash()),
     },
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/css/app-lite.css',
-                'resources/css/font.css',
-                'resources/js/app.js',
-                'resources/js/kao-logic.js'
+                "resources/css/app.css",
+                "resources/css/app-lite.css",
+                "resources/css/font.css",
+                "resources/js/app.js",
+                "resources/js/kao-logic.js",
             ],
             refresh: true,
         }),
     ],
     server: {
+        cors: true,
         host: true,
         hmr: {
-            host: '192.168.0.2'
+            host: "192.168.0.2",
         },
     },
 });
