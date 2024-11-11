8891dc0c4595051aa26e95d0bd3815a7a6f6cdb8
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2024-11-11 07:27:44 +0700

    Helpers: Check to all related column while generate UUID
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- app/Helpers/AppHelper.php
+++ app/Helpers/AppHelper.php
@@ -2,7 +2,14 @@
 namespace App\Helpers;
 
 use App\Models\appLogModel;
+use App\Models\jobLogModel;
 use App\Models\notifyLogModel;
+use App\Models\compressModel;
+use App\Models\cnvModel;
+use App\Models\htmlModel;
+use App\Models\mergeModel;
+use App\Models\splitModel;
+use App\Models\watermarkModel;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
@@ -70,59 +77,29 @@ class AppHelper
         ftp_close($ftp_conn);
     }
 
-    function getGitCommitHash()
-    {
-        $gitSourcePath = base_path('.git/');
-
-        if (!file_exists($gitSourcePath)) {
-            return null;
-        }
-
-        $headFile = $gitSourcePath.'HEAD';
-        $head = trim(file_get_contents($headFile));
-
-        if (strpos($head, 'ref: ') === 0) {
-            $ref = substr($head, 5);
-            $commitHashFile = $gitSourcePath . $ref;
-
-            if (!file_exists($commitHashFile)) {
-                return null;
-            }
-
-            $hash = trim(file_get_contents($commitHashFile));
-        } else {
-            $hash = $head;
-        }
-
-        return substr($hash, 0, 7);
-    }
-
-    function generateSingleUniqueUuid($customModel, $customColumn) {
-        $startProc = Carbon::now()->format('Y-m-d H:i:s');
-        $uniqueID = Uuid::uuid4();
-        // do {
-        //     $uniqueID = Uuid::uuid4();
-        // } while (
-        //     $customModel::where($customColumn, $uniqueID)->exists()
-        // );
-        $end = Carbon::now();
-        $duration = $end->diffInSeconds(Carbon::parse($startProc));
-        Log::Info('New single unique UUID has been generated with response time: '.$duration.' seconds');
-        return $uniqueID->toString();
-    }
-
     function generateUniqueUuid($customModel, $customColumn) {
-        $startProc = Carbon::now()->format('Y-m-d H:i:s');
         $uniqueID = Uuid::uuid4();
-        // do {
-        //     $uniqueID = Uuid::uuid4();
-        // } while (
-        //     appLogModel::where($customColumn, $uniqueID)->exists() ||
-        //     $customModel::where($customColumn, $uniqueID)->exists()
-        // );
-        $end = Carbon::now();
-        $duration = $end->diffInSeconds(Carbon::parse($startProc));
-        Log::Info('New unique UUID has been generated with response time: '.$duration.' seconds');
+        // if ($customColumn !== 'processId') {
+        //     do {
+        //         $uniqueID = Uuid::uuid4();
+        //     } while (
+        //         $customModel::where($customColumn, $uniqueID)->exists()
+        //     );
+        // } else {
+        //     do {
+        //         $uniqueID = Uuid::uuid4();
+        //     } while (
+        //         appLogModel::where($customColumn, $uniqueID)->exists() ||
+        //         jobLogModel::where($customColumn, $uniqueID)->exists() ||
+        //         notifyLogModel::where($customColumn, $uniqueID)->exists() ||
+        //         compressModel::where($customColumn, $uniqueID)->exists() ||
+        //         cnvModel::where($customColumn, $uniqueID)->exists() ||
+        //         htmlModel::where($customColumn, $uniqueID)->exists() ||
+        //         mergeModel::where($customColumn, $uniqueID)->exists() ||
+        //         splitModel::where($customColumn, $uniqueID)->exists() ||
+        //         watermarkModel::where($customColumn, $uniqueID)->exists()
+        //     );
+        // }
         return $uniqueID->toString();
     }
 
