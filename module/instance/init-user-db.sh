86d3f62f27f90bf1144de7200e4831d110a43294
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2024-11-27 16:12:31 +0700

    Treewide: Docker: Introduce Minio S3 Object Storage !
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- module/instance/init-user-db.sh
+++ module/instance/init-user-db.sh
@@ -2,6 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE hanaci_pdf;
     CREATE USER eureka WITH PASSWORD 'eureka';
     GRANT USAGE, CREATE ON SCHEMA public TO eureka;
     ANAYLYZE;
