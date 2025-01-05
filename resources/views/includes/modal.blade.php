c4238b41dae08a51e8823bf37c0388fa092fbd9c
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2025-01-05 14:48:42 +0700

    Views: Minor changes for text sizes on pop up message
    Signed-off-by: Dicky Herlambang (花） <herlambangdicky5@gmail.com>

--- resources/views/includes/modal.blade.php
+++ resources/views/includes/modal.blade.php
@@ -28,7 +28,7 @@
             <div class="p-6 space-y-6">
                 <img class="mx-auto mt-4 mb-4 text-dt3 w-12 h-12" src="{{ asset('assets/icons/exclamation.svg') }}" alt="exclamation">
                 <p id="errMessageModal" class="font-quicksand font-semibold text-dt1 text-lg leading-relaxed text-center">....</p>
-                <p id="errSubMessageModal" class="font-quicksand font text-dt3 text-xs leading-relaxed text-center">
+                <p id="errSubMessageModal" class="font-quicksand font text-dt3 text-sm leading-relaxed text-center">
                     <div id="altSubMessageModal" class="flex p-4 mb-4 text-sm text-lt1 bg-rt1 rounded-md" role="alert" style="display: none;">
                         <svg class="flex-shrink-0 inline w-4 h-4 me-3 mt-[2px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                             <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"></path>
